2eadb7a4a927652d1d729461c9dc6adfe3ec8028
MaryOp <mar-opolchentseva@yandex.ru>
2022-05-22 20:49:03 +0300

    feat: 05-merge-styles ready (Вс, мая 22, 2022 21:21:51)

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+
+let pathToFile = path.dirname(__filename);
+let secretPath = path.join(pathToFile,'/secret-folder');
+
+try {
+  fs.readdir(secretPath, (err, data) => {
+    if (err)
+      throw err;
+    data.forEach(file => {
+      fs.stat(path.join(secretPath, file), (err, stats) => {
+        if (err) throw err;
+        if  (stats.isFile()) {
+          let result = (path.basename(file, path.extname(file)) + ' - ' + path.extname(file) + ' - ' + stats['size']/1024)+' Kb';
+          console.log(result);
+        }
+      });
+    });
+  });
+}
+catch (error) {
+  console.log(error);
+}
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
+
+
+let pathToFile = path.dirname(__filename);
+let newPath = path.join(pathToFile, '/project-dist');
+
+let ws = fs.createWriteStream(path.join(newPath, 'bundle.css'));
+
+try {
+  fs.readdir(path.join(pathToFile,'/styles'), (err, data) => {
+    if (err) throw err;
+    data.forEach(file => {
+      
+      if (path.extname(file) == '.css') {
+        let rs = fs.createReadStream(path.join(pathToFile,'/styles',file));
+        rs.on('error', function(err) {
+          console.log(err);
+        });
+        ws.on('error', function(err) {
+          console.log(err);
+        });
+        ws.on('close', () => {
+          process.exit();
+        });
+
+        rs.pipe(ws);
+
+      }
+    });
+  });
+}
+
+catch (error) {
+  console.log(error);
+}
