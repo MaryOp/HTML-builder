72481becd28d22d0f9d85931e98f307d5618e8fc
MaryOp <mar-opolchentseva@yandex.ru>
2022-05-22 20:05:12 +0300

    feat: 04-copy-directory ready  (Вс, мая 22, 2022 20:05:12)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+// const process = require('process');
 
+
+let pathToFile = path.dirname(__filename);
+let newPath = path.join(pathToFile, 'files-copy');
+
+fs.mkdir(newPath, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+try {
+  fs.readdir(path.join(pathToFile,'/files'), (err, data) => {
+    if (err) throw err;
+    data.forEach(file => {
+      // console.log(file + '\n' + path.join(newPath, file));
+      fs.copyFile(path.join(pathToFile,'/files',file), path.join(newPath, file), () => {
+        if (err) throw err;
+      });
+    });
+
+  });
+  console.log('Копирование папки files завершено');
+}
+
+catch (error) {
+  console.log(error);
+}
